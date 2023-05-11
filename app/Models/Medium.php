8b5fbf8be8aa3a622b152cfffb5fb807cf340dc5
Shiraki <stgtbike4649@gmail.com>
2023-05-11 11:11:16 +0900

    create model file about media

--- app/Models/Medium.php
+++ app/Models/Medium.php
@@ -12,4 +12,38 @@ class Medium extends Model
     protected $fillable = [
         'medium_name'
     ];
+
+    /**
+     * Get all Records order by desc
+     *
+     * @return void
+     */
+    public function get_all_records()
+    {
+        $all_data = $this->orderBy('id', 'desc')->get();
+
+        return $all_data;
+    }
+
+
+
+    /**
+     * Get records with making where query
+     *
+     * @param string|null $name
+     * @param string|null $id
+     * @return void
+     */
+    public function get_records(string $name = null, string $id = null)
+    {
+        if ($name !== null && $id !== null) {
+            $records = $this->where('medium_name', 'LIKE', '%' . $name . '%')->where('id', 'LIKE', '%' . $id . '%')->orderBy('id', 'desc')->get();
+        } elseif ($name !== null && $id === null) {
+            $records = $this->where('medium_name', 'LIKE', '%' . $name . '%')->orderBy('id', 'desc')->get();
+        } elseif ($name === null && $id !== null) {
+            $records = $this->where('id', 'LIKE', '%' . $id . '%')->orderBy('id', 'desc')->get();
+        }
+
+        return $records;
+    }
 }
