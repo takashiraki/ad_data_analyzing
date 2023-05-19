13426f2b75403e79aaadcc228bbbc06f14d1613f
Shiraki <stgtbike4649@gmail.com>
2023-05-19 10:11:23 +0900

    edit routing

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Medium\MediumController;
 use App\Http\Controllers\Medium\AddMediumController;
 use App\Http\Controllers\Medium\SearchMediumController;
+use App\Http\Controllers\MediumDtl\AddMediumDtlController;
 use App\Http\Controllers\MediumDtl\MediumDtlController;
+use App\Http\Controllers\MediumDtl\SearchMediumDtlController;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,4 +32,9 @@ Route::post('/medium/store', [AddMediumController::class, 'handle']);
 
 Route::get('/medium/search', [SearchMediumController::class, 'index']);
 
-Route::get('medium-dtls', [MediumDtlController::class, 'index']);
+Route::get('/medium-dtls', [MediumDtlController::class, 'index']);
+
+Route::get('/medium-dtls/create', [AddMediumDtlController::class, 'index']);
+Route::post('/medium-dtls/store', [AddMediumDtlController::class, 'handle']);
+
+Route::get('/medium-dtls/search', [SearchMediumDtlController::class, 'index']);
