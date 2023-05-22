6db926f05e721a2adb51c63648fec36706af2559
Shiraki <stgtbike4649@gmail.com>
2023-05-22 17:29:08 +0900

    refactoring

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,12 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Medium\MediumController;
 use App\Http\Controllers\Medium\AddMediumController;
+use App\Http\Controllers\Medium\DeleteMediumController;
+use App\Http\Controllers\Medium\EditMediumController;
 use App\Http\Controllers\Medium\SearchMediumController;
 use App\Http\Controllers\MediumDtl\AddMediumDtlController;
+use App\Http\Controllers\mediumDtl\DeleteMediumDtlController;
+use App\Http\Controllers\mediumDtl\EditMediumDtlController;
 use App\Http\Controllers\MediumDtl\MediumDtlController;
 use App\Http\Controllers\MediumDtl\SearchMediumDtlController;
 
@@ -25,6 +29,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// About media screen
 Route::get('/media', [MediumController::class, 'index']);
 
 Route::get('/medium/create', [AddMediumController::class, 'index']);
@@ -32,9 +37,20 @@ Route::post('/medium/store', [AddMediumController::class, 'handle']);
 
 Route::get('/medium/search', [SearchMediumController::class, 'index']);
 
+Route::get('/medium/{medium_id}/edit', [EditMediumController::class, 'index']);
+Route::post('/medium/{medium_id}/update', [EditMediumController::class, 'handle']);
+
+Route::get('medium/{medium_id}/delete', [DeleteMediumController::class, 'index']);
+
+// About medium details screen
 Route::get('/medium-dtls', [MediumDtlController::class, 'index']);
 
 Route::get('/medium-dtls/create', [AddMediumDtlController::class, 'index']);
 Route::post('/medium-dtls/store', [AddMediumDtlController::class, 'handle']);
 
 Route::get('/medium-dtls/search', [SearchMediumDtlController::class, 'index']);
+
+Route::get('/medium-dtls/{medium_dtl_id}/edit', [EditMediumDtlController::class, 'edit']);
+Route::post('/medium-dtls/{medium_dtl_id}/update', [EditMediumDtlController::class, 'update']);
+
+Route::get('/medium-dtls/{medium_dtl_id}/delete', [DeleteMediumDtlController::class, 'delete']);
