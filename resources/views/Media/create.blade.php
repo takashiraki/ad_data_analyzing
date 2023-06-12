fc3724289c32e5ca18af68ec1f1bb1443c713d1e
nakajimakaede <nakajima.k@plusx-x.co.jp>
2023-06-12 05:57:28 +0000

    creating media screen

--- resources/views/Media/create.blade.php
+++ resources/views/Media/create.blade.php
@@ -2,11 +2,24 @@
 @section('title', '媒体登録')
 
 @section('contents')
-    <form action="/medium/store" method="post">
-        @csrf
-        <label>媒体名</label>
-        <input type="text" name="medium_name">
 
-        <button type="submit">登録する</button>
-    </form>
+    <section id="bords">
+    
+        <div id="add" class="wrap">
+            <div class="inner">
+                <a class="back-btn" href="./list.html"><p><i class="fa-solid fa-angles-left"></i></p></a>
+                <h3 class="hl-03"><img src="../image/btn-add.svg" alt="add">媒体登録<a href=""></a></h3>
+                <form method="post" class="add-form" action="/medium/store">
+                    @csrf
+                    <label>
+                        <span>媒体名</span>
+                        <input type="search" class="add-field" placeholder="" name="medium_name">
+                    </label>
+                    <input type="submit" class="add-submit" value="登録する">
+                </form>
+            </div>
+        </div>
+
+    </section>
+
 @endsection
