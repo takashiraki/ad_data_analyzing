54f52fc4f3e022bf1ae9521351b03ee9f93ca8e5
nakajimakaede <nakajima.k@plusx-x.co.jp>
2023-06-12 05:57:28 +0000

    fixed conflict of app.css

---
+++ resources/views/layout/common.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <meta charset="UTF-8">
+
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta http-equiv="Pragma" content="no-cache">
+        <meta http-equiv="cache-control" content="no-cache">
+        <meta http-equiv="expires" content="0">
+        <meta http-equiv="Content-Style-Type" content="text/css">
+        <meta http-equiv="Content-Script-Type" content="text/javascript">
+        <meta name="description" content="@yield('description')">
+        <meta name="format-detection" content="telepfhone=no">
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0">
+        <title>@yield('title') | ADAS</title>
+        <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+        {{-- <link rel="stylesheet" href="/css/style.css"> --}}
+        <link rel="stylesheet" href="/js/table.js">
+        <link rel="stylesheet" href="/js/nav.js">
+        <script src="https://kit.fontawesome.com/60583edc38.js" crossorigin="anonymous"></script>
+        @vite(['resources/scss/style.scss', 'resources/js/app.js'])
+    </head>
+
+    <body>
+        @yield('header')
+        @yield('side')
+        @yield('contents')
+    </body>
+
+</html>
+
---
+++ resources/views/layout/side.blade.php
@@ -0,0 +1,18 @@
+@section('side')
+<section id="side">
+    <h1 class="hl-01">システム名とか入れる</h1>
+    <ul class="fa-ul">
+        <li><a href="../dashboard.html"><p><i class="fas fa-fw fa-grip-horizontal"></i>Dashboard</p></a></li>
+        <li><a href="../médium/list.html"><p><i class="fas fa-fw fa-laptop-code"></i>媒体管理</p></a></li>
+        <li><a href="../médium-dtl/list.html"><p><i class="fas fa-fw fa-network-wired"></i>媒体詳細管理</p></a></li>
+        <li><a href="../URL_management/list.html"><p><i class="fas fa-fw fa-link"></i>URL管理</p></a></li>
+        <li><a href="../LP_management/list.html"><p><i class="fas fa-fw fa-pager"></i>LP管理</p></a></li>
+        <li><a href="../form_management/list.html"><p><i class="fa-solid fa-fw fa-paper-plane"></i>Form管理</p></a></li>
+        <li><a href="../ad_data/management.html"><p><i class="fas fa-fw fa-chart-bar"></i>広告データ管理</p></a></li>
+        <li><a href="../merge_data_management"><p><i class="fas fa-fw fa-project-diagram"></i>統合データ管理</p></a></li>
+        <li><a href="../employee_management/list.html"><p><i class="fas fa-fw fa-user-friends"></i>従業員管理</p></a></li>
+        <li><a href="../client_management/list.html"><p><i class="fas fa-fw fa-users"></i>顧客管理</p></a></li>
+        <li><a href="../sells_data/management.html"><p><i class="fas fa-fw fa-chart-line"></i>営業データ管理</p></a></li>
+    </ul>
+</section>
+@endsection
