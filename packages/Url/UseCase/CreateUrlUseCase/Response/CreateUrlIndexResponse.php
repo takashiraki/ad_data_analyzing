3bf3effd9adc2f42dae93e8e8bb20fd70865e274
Takashi <stgtbike4649@gmail.com>
2023-07-28 02:49:05 +0000

    Added getter in the CreateUrlIndexResponse

--- packages/Url/UseCase/CreateUrlUseCase/Response/CreateUrlIndexResponse.php
+++ packages/Url/UseCase/CreateUrlUseCase/Response/CreateUrlIndexResponse.php
@@ -35,4 +35,24 @@ class CreateUrlIndexResponse
         $this->lps = $lps;
         $this->forms = $forms;
     }
+
+    public function getMedia(): ?array
+    {
+        return $this->media;
+    }
+
+    public function getMediaDtls(): ?array
+    {
+        return $this->media_dtls;
+    }
+
+    public function getLps(): ?array
+    {
+        return $this->lps;
+    }
+
+    public function getForms(): ?array
+    {
+        return $this->forms;
+    }
 }
