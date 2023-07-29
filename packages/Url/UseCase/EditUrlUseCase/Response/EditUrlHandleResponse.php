ef410f4bf6c3fdfebd8c13a65d9828bfd9340fe2
Takashi <stgtbike4649@gmail.com>
2023-07-29 10:16:04 +0000

    fixed propaty

--- packages/Url/UseCase/EditUrlUseCase/Response/EditUrlHandleResponse.php
+++ packages/Url/UseCase/EditUrlUseCase/Response/EditUrlHandleResponse.php
@@ -7,17 +7,61 @@ use Url\Domain\DomainService\ValueObject\UrlSummary;
 class EditUrlHandleResponse
 {
     /**
-     * @var UrlSummary
+     * @var string
      */
-    private $url_summary;
+    private $url_name;
 
-    public function __construct(UrlSummary $summary)
+    /**
+     * @var string
+     */
+    private $medium_name;
+
+    /**
+     * @var string
+     */
+    private $medium_dtl_name;
+
+    /**
+     * @var string
+     */
+    private $lp_name;
+
+    /**
+     * @var string
+     */
+    private $form_name;
+
+    public function __construct(UrlSummary $url_summary)
     {
-        $this->url_summary = $summary;
+        $this->url_name = $url_summary->getUrlName()->getValue();
+        $this->medium_name = $url_summary->getMedium()->getMediumName()->getValue();
+        $this->medium_dtl_name = $url_summary->getMediumDtl()->getMediumDtlName()->getValue();
+        $this->lp_name = $url_summary->getLp()->getLpName()->getValue();
+        $this->form_name = $url_summary->getForm()->getFormName()->getValue();
     }
 
-    public function getUrlSummary(): UrlSummary
+    public function getUrlName(): string
     {
-        return $this->url_summary;
+        return $this->url_name;
+    }
+
+    public function getMediumName(): string
+    {
+        return $this->medium_name;
+    }
+
+    public function getMediumDtlName(): string
+    {
+        return $this->medium_dtl_name;
+    }
+
+    public function getLpName(): string
+    {
+        return $this->lp_name;
+    }
+
+    public function getFormName(): string
+    {
+        return $this->form_name;
     }
 }
